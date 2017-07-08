127e97506a02159e8bf2c1782d4d04fb4ace5821
shikeyue <xinluqishi@126.com>
2017-07-08 11:00:43 +0800

    Change the minSDK number to fit the device testing

--- app/src/main/java/com/xinluqishi/myfirstapp/MainActivity.java
+++ app/src/main/java/com/xinluqishi/myfirstapp/MainActivity.java
@@ -1,5 +1,10 @@
 package com.xinluqishi.myfirstapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:12345678"));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
+
+
     }
 }
