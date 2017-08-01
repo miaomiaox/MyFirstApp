850064e83f3e2e1ae183b8370e5e17a9801a28f4
shikeyue <xinluqishi@126.com>
2017-08-01 14:04:08 +0800

    Coding the AIDL interface and implementation class by myself not by the IDE auto-running.

--- app/src/main/java/com/xinluqishi/myfirstapp/binder/impl/BookBinder.java
+++ app/src/main/java/com/xinluqishi/myfirstapp/binder/impl/BookBinder.java
@@ -2,6 +2,7 @@ package com.xinluqishi.myfirstapp.binder.impl;
 
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.Parcel;
 import android.os.RemoteException;
 
 import com.xinluqishi.myfirstapp.bean.Book;
@@ -20,17 +21,38 @@ public class BookBinder extends Binder implements IBookBinder {
         this.attachInterface(this, DESCRIPTOR);
     }
 
-//    public static IBookBinder asInterface(android.os.IBinder obj)
-//    {
-//        if ((obj==null)) {
-//            return null;
-//        }
-//        android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
-//        if (((iin!=null)&&(iin instanceof IBookBinder))) {
-//            return ((IBookBinder)iin);
-//        }
-//        return new BookBinder.Proxy(obj);
-//    }
+    public static IBookBinder asInterface(android.os.IBinder obj)
+    {
+        if ((obj==null)) {
+            return null;
+        }
+        android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
+        if (((iin!=null)&&(iin instanceof IBookBinder))) {
+            return ((IBookBinder)iin);
+        }
+        return new BookBinder.Proxy(obj);
+    }
+
+    @Override
+    public IBinder asBinder() {
+        return this;
+    }
+
+    @Override
+    protected boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+
+        switch (code) {
+            case INTERFACE_TRANSACTION: {
+                reply.writeString(DESCRIPTOR);
+                return true;
+            }
+            case TRANSACTION_getBookList: {
+                data.enforceInterface(DESCRIPTOR);
+            }
+        }
+
+        return false;
+    }
 
     @Override
     public List<Book> getBookList() throws RemoteException {
@@ -42,8 +64,5 @@ public class BookBinder extends Binder implements IBookBinder {
 
     }
 
-    @Override
-    public IBinder asBinder() {
-        return this;
-    }
+
 }
