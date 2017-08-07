01e426ef558957dd302cf3e80f468c978c92fefd
shikeyue <xinluqishi@126.com>
2017-08-07 09:30:47 +0800

    Coding login part based MVP model. Adding some style xml code.

---
+++ app/src/main/java/com/xinluqishi/myfirstapp/presenter/user/ILoginPresenter.java
@@ -0,0 +1,8 @@
+package com.xinluqishi.myfirstapp.presenter.user;
+
+/**
+ * Created by shikeyue on 17/8/7.
+ */
+
+public interface ILoginPresenter {
+}
--- app/src/main/java/com/xinluqishi/myfirstapp/ui/user/LoginActivity.java
+++ app/src/main/java/com/xinluqishi/myfirstapp/ui/user/LoginActivity.java
@@ -1,10 +1,39 @@
 package com.xinluqishi.myfirstapp.ui.user;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.xinluqishi.myfirstapp.presenter.user.ILoginPresenter;
 import com.xinluqishi.myfirstapp.ui.base.BaseActivity;
 
 /**
  * Created by shikeyue on 2017/8/6.
  */
 
-public class LoginActivity extends BaseActivity implements ILoginView {
+public class LoginActivity extends BaseActivity implements ILoginView, View.OnClickListener {
+
+    private EditText editUsername;
+    private EditText editUserpass;
+
+    private Button btnLogin;
+    private Button btnClear;
+
+    ILoginPresenter loginPresenter;
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
